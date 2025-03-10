adb1c77c3613e243c02ccee084465ac6a0df66f5
William Dunston <william.dunston@endava.com>
2025-03-10 15:42:03 -0400

    Fixed the tailwindcss console warnings

--- myApp/tailwind.config.js
+++ myApp/tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}',
-    "./node_modules/flowbite/**/*.js"
-  ],
+  content: ["./src/**/*.{html,ts, scss}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF5733", // Custom primary color (Orange-Red)
+        secondary: "#4A90E2", // Custom blue shade
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        ponomar: ["Ponomar", "serif"],
+      },
+    },
   },
-  plugins: [require('@tailwindcss/aspect-ratio')
-,require('@tailwindcss/forms')
-,require('@tailwindcss/line-clamp')
-,require('@tailwindcss/typography')
-,require('flowbite/plugin')
-],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("flowbite/plugin"),
+  ],
 };
